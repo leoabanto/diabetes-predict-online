2fd55dff013bfd1019d6db1abdca1fa292a4e385
Leonardo <leonardoabantollanos@gmail.com>
2023-06-22 00:12:57 -0500

    import project files

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+import joblib
 
 app = Flask(__name__)
+model = joblib.load('model.joblib')
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/predict', methods=['GET'])
+def predict():
+    user_input = request.args.get('user_input', '')
+    result = model.predict([[float(x) for x in user_input.split(',')]])
+    return jsonify({'result': result.tolist()})
